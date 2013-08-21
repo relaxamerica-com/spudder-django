30973feaf823ae2463c0a4aad9bce19cf5e2e317
Karol <karol.gebert@solwit.pl>
2013-08-21 18:40:36 +0200

    Fan profile in progress and switches

--- src/website/fan/urls.py
+++ src/website/fan/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('website.fan.views',
     (r'^spuds$', 'spuds'),
     (r'^fans$', 'fans'),
+    (r'^public/(?P<user_id>\d+)$', 'public_view'),
 )
--- src/website/fan/views.py
+++ src/website/fan/views.py
@@ -3,10 +3,15 @@ Created on 17-08-2013
 
 @author: doop
 '''
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 def spuds(request):
     return render(request, 'fan/spuds.html')
 
 def fans(request):
     return render(request, 'fan/fans.html')
+
+def public_view(request, user_id):
+    user = get_object_or_404(User, pk = user_id)
+    return render(request, 'fan/public_view.html', { 'user' : user })
