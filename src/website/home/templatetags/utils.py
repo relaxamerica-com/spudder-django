2776fd385922ddb4753d32af62c1d6cb4313e2cb
Karol <karol.gebert@solwit.pl>
2013-09-14 12:06:50 +0200

    Fixed footer and tournament bugs

--- src/website/home/templatetags/utils.py
+++ src/website/home/templatetags/utils.py
@@ -2,8 +2,8 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('commons/display_items.html')
-def display_items(div_id, title):
-    return { 'id' : div_id, 'title' : title }
+def display_items(div_id, title, is_sponsor_us = False):
+    return { 'id' : div_id, 'title' : title, 'is_sponsor_us' : is_sponsor_us }
 
 @register.inclusion_tag('commons/display_users.html')
 def display_users(users, orientation, max_users):
