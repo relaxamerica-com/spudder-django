9bd639ccbafccb55c4cd443febe467b3d9517c82
Lukasy Rybka <darkstariw@gmail.com>
2013-11-17 13:32:29 +0100

    Amazon Flexible Payment System migration from Google App Engine to Parse. Added donations aggregated view.

--- src/cloud/keys.js
+++ src/cloud/keys.js
@@ -5,7 +5,8 @@ module.exports = function (version) {
             'karol': 2
         },
         applicationIds = ['RwjN7ubrqVZSXcwd2AWaQtov6Mgsi7hAXZ510xTR', 'QZjpmUJQEwBE6Wc0YfKEMRwv2C5Aeb4qQbopyIg9', 'YU4g6sCW8Dvl6khJsVYgXhr20Pu5zaaLcIQ4oRON'],
-        javascriptKeys = ['zDk1PxddnEJwnLKxrnypGuM4pIq9Z7adAi4rprgH', 'UygLrj7cUKeLSxDWOd7Ch4bEyLfBxQglf2VkGc6Q', 'JRu7BtIEuRDl3QaYNRLguV4a1pFjES02wHfRP5Al'];
+        javascriptKeys = ['zDk1PxddnEJwnLKxrnypGuM4pIq9Z7adAi4rprgH', 'UygLrj7cUKeLSxDWOd7Ch4bEyLfBxQglf2VkGc6Q', 'JRu7BtIEuRDl3QaYNRLguV4a1pFjES02wHfRP5Al'],
+        appNames = ['spudmart', 'spudmartlukasz', 'karol'];
 
     return {
         getJavaScriptKey: function () {
@@ -14,6 +15,13 @@ module.exports = function (version) {
 
         getApplicationID: function () {
             return applicationIds[versions[version]];
-        }
+        },
+
+        getAppName: function () {
+            return appNames[versions[version]];
+        },
+
+        AWS_ACCESS_KEY_ID: 'AKIAIV6G36242PJ4L7WQ',
+        AWS_SECRET_KEY_ID: '5EIFJLrQB6RhtI4wOBLF7DPu4ZyTMbOrwXrneJZg'
     };
 };
--- src/cloud/modules.js
+++ src/cloud/modules.js
@@ -10,9 +10,19 @@ module.exports = function (app, keys) {
     var tournament = require('cloud/tournament/actions');
     app.get('/tournament', tournament.view);
 
-    var dashboard = require('cloud/dashboard/actions');
+    var dashboard = require('cloud/dashboard/actions')(keys);
     app.get('/dashboard', dashboard.spuds);
     app.get('/dashboard/general', dashboard.general);
+
+    app.get('/dashboard/recipient', dashboard.recipient);
+    app.post('/dashboard/recipient/save', dashboard.recipient_save);
+    app.get('/dashboard/recipient/complete', dashboard.recipient_complete);
+
+    app.get('/dashboard/sponsor', dashboard.sponsor);
+    app.post('/dashboard/sponsor/confirm', dashboard.sponsor_confirm);
+    app.get('/dashboard/sponsor/complete', dashboard.sponsor_complete);
+
+    app.get('/dashboard/donations', dashboard.list_donations);
 
     var spudmart = require('cloud/spudmart/actions');
     app.get('/spudmart', spudmart.view);
