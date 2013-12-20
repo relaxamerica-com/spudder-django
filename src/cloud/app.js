bee604f128c3657dbee9986b31492250db7e46e1
Smoczysko <darkstariw@gmail.com>
2013-12-20 19:26:44 +0100

    App.js

--- src/cloud/app.js
+++ src/cloud/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var keys = require('cloud/keys.js')('lukasz');
+var keys = require('cloud/keys.js')('spudmart');
 
 require('cloud/config.js')(app, express);
 require('cloud/middleware.js')(app, keys);
