5fca482dbd2ef6cf34959d3c393b49ee47fea786
Smoczysko <darkstariw@gmail.com>
2013-12-20 19:54:51 +0100

    Fix

--- src/cloud/keys.js
+++ src/cloud/keys.js
@@ -1,4 +1,4 @@
-module.exports = function (version) {
+module.exports = function () {
     var versions = {
             'spudmart': 0,
             'lukasz': 1,
@@ -6,7 +6,13 @@ module.exports = function (version) {
         },
         applicationIds = ['RwjN7ubrqVZSXcwd2AWaQtov6Mgsi7hAXZ510xTR', 'QZjpmUJQEwBE6Wc0YfKEMRwv2C5Aeb4qQbopyIg9', 'YU4g6sCW8Dvl6khJsVYgXhr20Pu5zaaLcIQ4oRON'],
         javascriptKeys = ['zDk1PxddnEJwnLKxrnypGuM4pIq9Z7adAi4rprgH', 'UygLrj7cUKeLSxDWOd7Ch4bEyLfBxQglf2VkGc6Q', 'JRu7BtIEuRDl3QaYNRLguV4a1pFjES02wHfRP5Al'],
-        appNames = ['spudmart', 'spudmartlukasz', 'karol'];
+        appNames = ['spudmart', 'spudmartlukasz', 'karol'], version = 'spudmart';
+
+    try {
+        version = require('cloud/private-key.js')();
+    } catch (_) {
+        // There is no .key.js file which means that main app should be loaded
+    }
 
     return {
         getJavaScriptKey: function () {
