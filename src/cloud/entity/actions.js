d250ff573185810cbc4ac3f3f66ab8047c7bc794
Smoczysko <darkstariw@gmail.com>
2014-01-23 21:39:16 +0100

    Added displaying team name on Player and Coach public pages. Resolves #15.

--- src/cloud/entity/actions.js
+++ src/cloud/entity/actions.js
@@ -3,46 +3,39 @@ module.exports = function (keys) {
     return {
         view: function (req, res) {
             var id = req.params.id,
-            	entityType = req.params.entityType,
+                entityType = req.params.entityType,
                 Entity = Parse.Object.extend(entityType),
                 query = new Parse.Query(Entity);
 
             query.get(id, {
                 success: function (entity) {
-					function _renderView(team) {
-	                    var path = 'https://' + keys.getAppName() + '.parseapp.com/public/' + entityType + '/' + id;
-	                    res.render(entityType.toLowerCase() + '/view', {
-	                        'displaySponsors' : require('cloud/commons/displaySponsors'),
-	                        'entity': entity,
-	                        'twitterShareButton': require('cloud/commons/twitterShareButton'),
-	                        'googlePlusShareButton': require('cloud/commons/googlePlusShareButton'),
-	                        'facebookShareButton': require('cloud/commons/facebookShareButton'),
+                    function _renderView(team) {
+                        var path = 'https://' + keys.getAppName() + '.parseapp.com/public/' + entityType + '/' + id;
+                        res.render(entityType.toLowerCase() + '/view', {
+                            'displaySponsors' : require('cloud/commons/displaySponsors'),
+                            'entity': entity,
+                            'twitterShareButton': require('cloud/commons/twitterShareButton'),
+                            'googlePlusShareButton': require('cloud/commons/googlePlusShareButton'),
+                            'facebookShareButton': require('cloud/commons/facebookShareButton'),
                             'emailShareButton': require('cloud/commons/emailShareButton'),
-	                        'meta': {
-	                            title: entity.get('name'),
-	                            description: entity.get('profile'),
-	                            image: entity.get('profileImageThumb') ? entity.get('profileImageThumb') : '',
-	                            url: path
-	                        },
-	                        'returnURL': path,
-	                        'team' : team
-	                	});
-                	}
-                	
-                	if (entity.get('team')) {
-	                	var Team = Parse.Object.extend('Team'),
-	                		teamQuery = new Parse.Query(Team);
-	                		
-	                	teamQuery.equalTo('name', entity.get('team'));
-	                	
-	                	teamQuery.first().then(function(team) {
-	                		_renderView(team);
-	                	});
-                		
-                	} else {
-	                	_renderView(null);
-                	}
-                	
+                            'meta': {
+                                title: entity.get('name'),
+                                description: entity.get('profile'),
+                                image: entity.get('profileImageThumb') ? entity.get('profileImageThumb') : '',
+                                url: path
+                            },
+                            'returnURL': path,
+                        'team' : team
+                       });
+                    }
+
+                    if (entity.get('team')) {
+                        entity.get('team').fetch().then(function (_team) {
+                            _renderView(_team);
+                        });
+                    } else {
+                       _renderView(null);
+                    }
                 },
                 error: function(object, error) {
                     res.render(entityType.toLowerCase() + '/view');
