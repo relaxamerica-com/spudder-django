f6cafc7b7e9fb303d73c5f7d9654c3197938feaa
Smoczysko <darkstariw@gmail.com>
2014-02-23 15:00:56 +0100

    Spudmart application - initial Django on Google App Engine app sceleton. Re #78.

---
+++ spudmart/src/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+urlpatterns = patterns('',
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    (r'^$', include('spudmart.home.urls')),
+    (r'^accounts/', include('registration.urls')),
+)
