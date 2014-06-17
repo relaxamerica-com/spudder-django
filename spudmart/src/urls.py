23ad9a7874346ee34272ed8cf566226bd420fe46
Matt Griffiths <mrmatthewgriffiths@gmail.com>
2014-06-17 16:53:19 +0100

    * Root all root url calls to the cern landing page

--- spudmart/src/urls.py
+++ spudmart/src/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls.defaults import *
-
+from django.views.generic.simple import redirect_to
 
 handler404 = 'django.views.defaults.page_not_found'
 handler500 = 'djangotoolbox.errorviews.server_error'
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^_ah/start$', 'spudmart.utils.backends.start'),
-    (r'^$', include('spudmart.home.urls')),
+    # (r'^$', include('spudmart.home.urls')),  NOTE: Commented out by MG 20140617
     (r'^dashboard/recipient/', include('spudmart.recipients.urls')),
     (r'^dashboard/donation/', include('spudmart.donations.urls')),
     (r'^dashboard/sponsor/', include('spudmart.sponsors.urls')),
@@ -16,9 +17,13 @@ urlpatterns = patterns('',
     (r'^venues/', include('spudmart.venues.urls')),
     (r'^spudder/', include('spudmart.spudder.urls')),
     (r'^upload/', include('spudmart.upload.urls')),
-    (r'^hospitals/', include('spudmart.hospitals.urls')),
     (r'^sponsor/', include('spudmart.sponsors.public_urls')),
+    (r'^hospitals/', include('spudmart.hospitals.urls')),
     (r'^file/serve/(?P<file_id>\d+)$', 'spudmart.upload.views.serve_uploaded_file'),
     (r'^api/1/', include('spudmart.api.urls')),
+
     (r'^cern/', include('spudmart.CERN.urls')),
+
+    # Note the below line was added to catch root urls and push them to CERN, MG 20140617
+    (r'^$', redirect_to, {'url': '/cern'})
 )
