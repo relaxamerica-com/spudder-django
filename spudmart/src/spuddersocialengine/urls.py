84ff645f8a2077de9bfb89c45efe8077b42bec3d
Matt Griffiths <mrmatthewgriffiths@gmail.com>
2014-07-11 16:41:32 +0100

    * removed code from the venue page * updated th eincludes in the socialengine urls file

--- spudmart/src/spuddersocialengine/urls.py
+++ spudmart/src/spuddersocialengine/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
 from spuddersocialengine.api import *
+from spuddersocialengine.socialnetworks.instagram import callback, callback_task
+
 
 urlpatterns = patterns(
     '',
-
     # API handlers
     url(r'^api/location$', location),
     url(r'^api/location_task$', location_task),
     url(r'^api/manual_process$', manual_process),
     url(r'^api/$', api_landing),
-    url(r'^api/instagram/', include('spuddersocialengine.socialnetworks.instagram_urls')),
+    url(r'^api/instagram/callback$', callback),
+    url(r'^api/instagram/callback_task$', callback_task),
 )
