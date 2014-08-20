1fde74c14b50bb7d8d31f27cbffa279c8b7f754c
Matt Griffiths <mrmatthewgriffiths@gmail.com>
2014-08-20 18:57:28 +0100

    * changes to the jumbotron styling * added logging to atpostspud

--- spudmart/src/spuddersocialengine/urls.py
+++ spudmart/src/spuddersocialengine/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, url
-from spuddersocialengine.locationscraper.api import *
+from spuddersocialengine.locationscraper import api
 from spuddersocialengine.locationscraper.socialnetworks.instagram import callback, callback_task
 from atpostspud.views import tick, get_latest_at_post_spuds_view
 
@@ -7,10 +7,10 @@ from atpostspud.views import tick, get_latest_at_post_spuds_view
 urlpatterns = patterns(
     '',
     # API handlers
-    url(r'^api/location$', location),
-    url(r'^api/location_task$', location_task),
-    url(r'^api/manual_process$', manual_process),
-    url(r'^api/$', api_landing),
+    url(r'^api/location$', api.location),
+    url(r'^api/location_task$', api.location_task),
+    url(r'^api/manual_process$', api.manual_process),
+    url(r'^api/$', api.api_landing),
     url(r'^api/instagram/callback$', callback),
     url(r'^api/instagram/callback_task$', callback_task),
 
