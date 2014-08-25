0e75da9a9a6a9f3a914f167ee32b4042b6fafb06
Lucy Moss <thecoloryes@gmail.com>
2014-08-25 12:06:38 -0400

    Implemented feature tags for spud_tagging

--- spudmart/src/spudderadmin/templatetags/featuretags.py
+++ spudmart/src/spudderadmin/templatetags/featuretags.py
@@ -2,6 +2,6 @@ from django.conf import settings
 from django.template.defaulttags import register
 
 
-@register.simple_tag(takes_context=True)
-def feature_is_enabled(context, feature):
+@register.filter()
+def feature_is_enabled(feature):
     return feature in settings.FEATURES[settings.ENVIRONMENT]
