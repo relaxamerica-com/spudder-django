61bd035cf58f05974989bb878c2b68fca9a7c72d
Matt Griffiths <mrmatthewgriffiths@gmail.com>
2014-09-24 13:16:11 +0100

    * working on the user flow in challenges

--- src/spudderspuds/challenges/urls.py
+++ src/spudderspuds/challenges/urls.py
@@ -4,32 +4,6 @@ from spudderdomain.models import TempClub, Club
 urlpatterns = patterns(
     'spudderspuds.challenges.views',
 
-    url(r'c(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)/o/(?P<club_id>\d+)$',
-        'challenge_accept_beneficiary_set_donation',
-        {'club_class': Club}),
-
-    url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)/t/(?P<club_id>\d+)$',
-        'challenge_accept_beneficiary_set_donation',
-        {'club_class': TempClub}),
-
-    url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)/create_club$',
-        'challenge_accept_beneficiary_create_club'),
-
-    url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)$',
-        'challenge_accept_beneficiary'),
-
-    url(r'(?P<participation_id>\d+)/state$',
-        'challenge_accept_state'),
-
-    url(r'(?P<challenge_id>\d+)/accept$',
-        'challenge_accept'),
-
-    url(r'(?P<challenge_id>\d+)/share$',
-        'challenge_share'),
-
-    url(r'(?P<challenge_id>\d+)$',
-        'challenge_view'),
-
     url(r'create/(?P<template_id>\d+)/(?P<state>\w+)/o/(?P<club_id>\d+)$',
         'create_challenge_set_donation',
         {'club_class': Club}),
@@ -55,4 +29,30 @@ urlpatterns = patterns(
 
     url(r'create$',
         'create_challenge'),
+
+    url(r'c(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)/o/(?P<club_id>\d+)$',
+        'challenge_accept_beneficiary_set_donation',
+        {'club_class': Club}),
+
+    url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)/t/(?P<club_id>\d+)$',
+        'challenge_accept_beneficiary_set_donation',
+        {'club_class': TempClub}),
+
+    url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)/create_club$',
+        'challenge_accept_beneficiary_create_club'),
+
+    url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)$',
+        'challenge_accept_beneficiary'),
+
+    url(r'(?P<participation_id>\d+)/state$',
+        'challenge_accept_state'),
+
+    url(r'(?P<challenge_id>\d+)/accept$',
+        'challenge_accept'),
+
+    url(r'(?P<challenge_id>\d+)/share$',
+        'challenge_share'),
+
+    url(r'(?P<challenge_id>\d+)$',
+        'challenge_view'),
 )
