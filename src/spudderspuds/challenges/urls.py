a76014e6ea0a9ceb8cda65eb56358ac3f85e2403
Vad <vadim@spuder.com>
2014-10-09 16:50:31 +0300

    [Finished #80243148] All challenge forms are HTTPS

--- src/spudderspuds/challenges/urls.py
+++ src/spudderspuds/challenges/urls.py
@@ -1,99 +1,100 @@
 from django.conf.urls.defaults import *
 from spudderdomain.controllers import EntityController
 from spudderdomain.models import TempClub, Club
+from spudmart.utils.url import determine_ssl
 
 urlpatterns = patterns(
     'spudderspuds.challenges.views',
 
     url(r'signin$',
-        'signin'),
+        'signin', {'SSL': determine_ssl()}),
 
     url(r'register$',
-        'register'),
+        'register', {'SSL': determine_ssl()}),
 
     url(r'register/team$',
-        'register_club'),
+        'register_club', {'SSL': determine_ssl()}),
 
     url(r'create/(?P<template_id>\d+)/(?P<state>\w+)/o/(?P<club_id>\d+)$',
         'create_challenge_set_donation',
-        {'club_class': Club}),
+        {'club_class': Club, 'SSL': determine_ssl()}),
 
     url(r'create/(?P<template_id>\d+)/(?P<state>\w+)/t/(?P<club_id>\d+)$',
         'create_challenge_set_donation',
-        {'club_class': TempClub}),
+        {'club_class': TempClub, 'SSL': determine_ssl()}),
 
     url(r'create/(?P<template_id>\d+)/(?P<state>\w+)/create_club$',
-        'create_challenge_choose_club_create_club'),
+        'create_challenge_choose_club_create_club', {'SSL': determine_ssl()}),
 
     url(r'create/(?P<template_id>\d+)/(?P<state>\w+)$',
-        'create_challenge_choose_club'),
+        'create_challenge_choose_club', {'SSL': determine_ssl()}),
 
     url(r'create/(?P<template_id>\d+)$',
-        'create_challenge_choose_club_choose_state'),
+        'create_challenge_choose_club_choose_state', {'SSL': determine_ssl()}),
 
     url(r'create$',
-        'create_challenge'),
+        'create_challenge', {'SSL': determine_ssl()}),
 
     url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)/o/(?P<club_id>\d+)$',
         'challenge_accept_beneficiary_set_donation',
-        {'club_class': Club}),
+        {'club_class': Club, 'SSL': determine_ssl()}),
 
     url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)/t/(?P<club_id>\d+)$',
         'challenge_accept_beneficiary_set_donation',
-        {'club_class': TempClub}),
+        {'club_class': TempClub, 'SSL': determine_ssl()}),
 
     url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)/create_club$',
-        'challenge_accept_beneficiary_create_club'),
+        'challenge_accept_beneficiary_create_club', {'SSL': determine_ssl()}),
 
     url(r'(?P<participation_id>\d+)/beneficiary/(?P<state>\w+)$',
-        'challenge_accept_beneficiary'),
+        'challenge_accept_beneficiary', {'SSL': determine_ssl()}),
 
     url(r'(?P<participation_id>\d+)/state$',
-        'challenge_accept_state'),
+        'challenge_accept_state', {'SSL': determine_ssl()}),
 
     url(r'challenge_challenge/(?P<participation_id>\d+)/upload$',
-        'challenge_challenge_accept_notice'),
+        'challenge_challenge_accept_notice', {'SSL': determine_ssl()}),
 
     url(r'challenge_challenge/beneficiary/(?P<state>\w+)/clubs/o/(?P<club_id>\d+)$',
         'challenge_challenge_accept_notice',
-        {'club_entity_type': EntityController.ENTITY_CLUB}),
+        {'club_entity_type': EntityController.ENTITY_CLUB, 'SSL': determine_ssl()}),
 
     url(r'challenge_challenge/beneficiary/(?P<state>\w+)/clubs/t/(?P<club_id>\d+)$',
         'challenge_challenge_accept_notice',
-        {'club_entity_type': EntityController.ENTITY_TEMP_CLUB}),
+        {'club_entity_type': EntityController.ENTITY_TEMP_CLUB, 'SSL': determine_ssl()}),
 
     url(r'challenge_challenge/beneficiary/(?P<state>\w+)/clubs/create_club$',
-        'challenge_challenge_accept_beneficiary_create_club'),
+        'challenge_challenge_accept_beneficiary_create_club', {'SSL': determine_ssl()}),
     
     url(r'challenge_challenge/beneficiary/(?P<state>\w+)/clubs$',
-        'challenge_challenge_accept_beneficiary_load_clubs'),
+        'challenge_challenge_accept_beneficiary_load_clubs', {'SSL': determine_ssl()}),
 
     url(r'challenge_challenge/beneficiary/(?P<state>\w*)$',
-        'challenge_challenge_accept_beneficiary'),
+        'challenge_challenge_accept_beneficiary', {'SSL': determine_ssl()}),
 
     url(r'challenge_challenge/(?P<participation_id>\d+)/thanks$',
-        'challenge_challenge_thanks'),
+        'challenge_challenge_thanks', {'SSL': determine_ssl()}),
 
     url(r'challenge_challenge$',
-        'challenge_challenge'),
+        'challenge_challenge', {'SSL': determine_ssl()}),
 
     url(r'(?P<challenge_id>\d+)/accept/notice$',
-        'challenge_accept_notice'),
+        'challenge_accept_notice', {'SSL': determine_ssl()}),
 
     url(r'(?P<challenge_id>\d+)/accept/pay$',
-        'challenge_accept_pay'),
+        'challenge_accept_pay', {'SSL': determine_ssl()}),
 
     url(r'(?P<challenge_id>\d+)/accept/pledge$',
-        'challenge_accept_pledge'),
+        'challenge_accept_pledge', {'SSL': determine_ssl()}),
 
     url(r'(?P<challenge_id>\d+)/accept$',
-        'challenge_accept_upload'),
+        'challenge_accept_upload', {'SSL': determine_ssl()}),
 
     url(r'(?P<challenge_id>\d+)/share$',
-        'challenge_share'),
+        'challenge_share', {'SSL': determine_ssl()}),
 
     url(r'(?P<challenge_id>\d+)$',
-        'challenge_view'),
+        'challenge_view', {'SSL': determine_ssl()}),
 
     url(r'^tick',
         'tick'),
@@ -102,5 +103,5 @@ urlpatterns = patterns(
         'send_challenge_emails'),
 
     url(r'$',
-        'challenges_splash'),
+        'challenges_splash', {'SSL': determine_ssl()}),
 )
